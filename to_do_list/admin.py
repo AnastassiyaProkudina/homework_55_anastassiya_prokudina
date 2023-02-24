dc8f4b0d1210ad55db1d47e56b96a40c14be41a1
Anastassiya Prokudina <anakess88@gmail.com>
2023-02-24 08:42:06 +0600

    Внесла изменения в регистрацию модели в админ панели

--- to_do_list/admin.py
+++ to_do_list/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from to_do_list.models import Task
 
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'title', 'status', 'date_to_do', 'description', 'created_at', 'updated_at', 'is_deleted',
+        'deleted_at')
+    list_filter = ('title', 'status', 'date_to_do')
+    fields = ('title', 'status', 'date_to_do', 'description')
+
+admin.site.register(Task, TaskAdmin)
